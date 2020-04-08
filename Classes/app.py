f4376bb73c7d0bdfb4be4165949c4773a8296627
madmelone <michaelfrey1984@gmail.com>
2020-04-08 13:46:17 +0200

    - [Change] Full flask structure in app.py and /templates

--- Classes/app.py
+++ Classes/app.py
@@ -1,13 +1,39 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('PlayerWorldranking.html')
+
 @app.route('/about/')
 def about():
-    page = request.args.get('page', default = 1, type = int)
-    filter = request.args.get('filter', default = '*', type = str)
-    return render_template('about.html')
+    return render_template('AppAbout.html')
+
+@app.route('/PlayerWorldranking/')
+def about():
+    #org = request.args.get('org', default = 1, type = str)
+    #type = request.args.get('type', default = '*', type = str)
+    #cut = request.args.get('cut', default = '*', type = int)
+    #language = request.args.get('language', default = '*', type = str)
+    #date = request.args.get('date', default = '*', type = str)
+    return render_template('PlayerWorldranking.html')
+
+@app.route('/TournamentDraw/')
+def about():
+    #org = request.args.get('org', default = 1, type = str)
+    #type = request.args.get('type', default = '*', type = str)
+    #year = request.args.get('cut', default = '*', type = str)
+    #language = request.args.get('language', default = '*', type = str)
+    return render_template('TournamentDraw.html')
+
+@app.route('/PlayerTourneywins/')
+def about():
+    #org = request.args.get('org', default = 1, type = str)
+    #type = request.args.get('type', default = '*', type = str)
+    #playerid = request.args.get('playerid', default = '*', type = str)
+    #language = request.args.get('language', default = '*', type = str)
+    return render_template('PlayerTourneywins.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
